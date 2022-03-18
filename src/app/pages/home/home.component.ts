b2720e7af248c7b21195747f7d856e890fb81b00
hieunm02 <nguyenminhhieu2002ptpt@gmail.com>
2022-03-18 23:05:25 +0700

    feat: update home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>("http://localhost:3000/subjects")
+      .subscribe(data => {
+        this.subjects = data;
+      });
   }
+  subjects: Array<any> = [
+
+  ]
 
 }
